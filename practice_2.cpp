87151aa575e202a3aa03379fb8bd4e45f5be8110
Tsuchiya <yuto.tsuchiya@sony.com>
2019-06-11 11:23:08 +0900

    コマンドライン引数で値を渡す

--- practice_2.cpp
+++ practice_2.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "longmath.h" 
+#include <stdlib.h>
 
-const int fact = 200; // fact階乗を計算する
+// const int fact = 200; // fact階乗を計算する
 
-int main(){
+int main(int argc, char* argv[]){
     Longint longbuf;
+    const int fact = atoi(argv[1]);
 
     for (int i=1; i<=fact; ++i){
         longbuf.multiply(i);
