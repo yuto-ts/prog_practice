b39cb2eecf97205cfbe4b75bc7962d6cad86e3c6
Tsuchiya <yuto.tsuchiya@sony.com>
2019-06-12 10:46:48 +0900

    set numbers

--- makeTen.cpp
+++ makeTen.cpp
@@ -6,40 +6,62 @@
 #include <math.h>
 #include <vector>
 
-class fraction
-{
+// class culc_fraction
+// {
+// public:
+//     culc_fraction& set(int, int = 1);
+//     culc_fraction& reduce(); // 約分
+//     culc_fraction& reduce(culc_fraction&); //通分
+//     culc_fraction& add(culc_fraction&);
+//     culc_fraction& sub(culc_fraction&);
+//     culc_fraction& mul(culc_fraction&);
+//     culc_fraction& div(culc_fraction&);
 
-};
+// private:
+//     int num;
+//     int den;
+// };
 
-class culc : public fraction
+class culc_Ten //: public culc_fraction
 {
 public:
-    culc& set();
-    culc& add(culc&, );
-    culc& sub();
-    culc& mul();
-    culc& div();
-    culc& solve();
+    culc_Ten& set_num(char);
+    auto culc_Ten::set_nums(culc_Ten *numbers, char argv);
+    culc_Ten& solve();
 private:
-    double num;
-    std::vector<int> buf{1};
+    int num;
+    std::vector<int> str{1};
 };
 
-void main(int argc, char **argv)
+culc_Ten& culc_Ten::set_num(char argv){
+    num = (int)(argv  - '0');
+    str[0] = argv;
+    str[1] = 0;
+    std::cout << num;
+
+    return *this;
+}
+
+auto culc_Ten::set_nums(culc_Ten *numbers, char argv){
+    for (int i = 0; i < 4; i++){
+        numbers[i].set_num(argv);
+    }
+}
+
+int main(int argc, char **argv)
 {
-    struct numstr numbers[4];
-    int i;
+    culc_Ten numbers[4];
 
     if ((argc != 2) || (strlen(argv[1]) != 4)) {
         printf("usage: %s (4 digits number)\n", argv[0]);
-        exit(1);
+        // exit(1);
+        return 0;
     }
 
-    for (i=0; i<4; i++) {
-        numbers[i].num = (double)(argv[1][i] - '0');
-        numbers[i].str[0] = argv[1][i];
-        numbers[i].str[1] = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        numbers[i].set_num(argv[1][i]);
     }
-
-    solve(numbers, 4);
+    
+    // solve(numbers, 4);
 }
---
+++ test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+
+
+#include <string.h>
+#include <math.h>
+#include <vector>
+
+// class culc_fraction
+// {
+// public:
+//     culc_fraction& set(int, int = 1);
+//     culc_fraction& reduce(); // 約分
+//     culc_fraction& reduce(culc_fraction&); //通分
+//     culc_fraction& add(culc_fraction&);
+//     culc_fraction& sub(culc_fraction&);
+//     culc_fraction& mul(culc_fraction&);
+//     culc_fraction& div(culc_fraction&);
+
+// private:
+//     int num;
+//     int den;
+// };
+
+class culc_Ten //: public culc_fraction
+{
+public:
+    // culc_Ten& solve();
+private:
+    double num;
+    std::vector<int> str{1};
+};
+
+int main(int argc, char **argv)
+{
+    culc_Ten number[4];
+    
+
+}
