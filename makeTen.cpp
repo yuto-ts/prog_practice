4a16c880308ddade405c13995df93413b20d88e9
Tsuchiya <yuto.tsuchiya@sony.com>
2019-06-13 09:44:07 +0900

    分数計算を宣言，実体定義

--- makeTen.cpp
+++ makeTen.cpp
@@ -6,27 +6,94 @@
 #include <math.h>
 #include <vector>
 
-// class culc_fraction
-// {
-// public:
-//     culc_fraction& set(int, int = 1);
-//     culc_fraction& reduce(); // 約分
-//     culc_fraction& reduce(culc_fraction&); //通分
-//     culc_fraction& add(culc_fraction&);
-//     culc_fraction& sub(culc_fraction&);
-//     culc_fraction& mul(culc_fraction&);
-//     culc_fraction& div(culc_fraction&);
+int gcd(int,int);
 
-// private:
-//     int num;
-//     int den;
-// };
+int gcd(int x, int y){
+    while(y){
+        int w = x % y;
+            x = y;
+            y = w;
+    }
+    return x;
+}
+
+class culc_fraction
+{
+public:
+    culc_fraction& set(int, int = 1);
+    culc_fraction& reduce(); // 約分
+    culc_fraction& reduce(culc_fraction&); //通分
+    culc_fraction& add(culc_fraction&);
+    culc_fraction& sub(culc_fraction&);
+    culc_fraction& mul(culc_fraction&);
+    culc_fraction& div(culc_fraction&);
+
+private:
+    int num;
+    int den;
+};
+
+culc_fraction& culc_fraction::set(int n, int d)
+{
+    num = n;
+    den = d;
+    reduce();
+    return *this;
+}
+
+culc_fraction& culc_fraction::reduce()
+{
+    int g = ::gcd(num, den);
+    num /= g;
+    den /= g;
+}
+
+culc_fraction& culc_fraction::reduce(culc_fraction& fb){
+    int d = den;
+    num *= fb.den;
+    den *= fb.den;
+    fb.den *= d;
+    fb.num *= d;
+    return *this;
+}
+
+culc_fraction& culc_fraction::add(culc_fraction& fb){
+    reduce(fb);
+    num += fb.num;
+    reduce();
+    fb.reduce();
+    return *this;
+}
+
+culc_fraction& culc_fraction::sub(culc_fraction& fb){
+    reduce(fb);
+    num -= fb.num;
+    reduce();
+    fb.reduce();
+    return *this;
+}
+
+culc_fraction& culc_fraction::mul(culc_fraction& fb){
+    num *= fb.num;
+    den *= fb.den;
+    reduce();
+    return *this;
+}
+
+culc_fraction& culc_fraction::div(culc_fraction& fb){
+    num *= fb.den;
+    den *= fb.num;
+    reduce();
+    return *this;
+}
+
+
 
 class culc_Ten //: public culc_fraction
 {
 public:
     culc_Ten& set_num(char);
-    auto culc_Ten::set_nums(culc_Ten *numbers, char argv);
+    auto      set_nums(culc_Ten *numbers, char argv);
     culc_Ten& solve();
 private:
     int num;
@@ -41,6 +108,8 @@ culc_Ten& culc_Ten::set_num(char argv){
 
     return *this;
 }
+
+
 
 auto culc_Ten::set_nums(culc_Ten *numbers, char argv){
     for (int i = 0; i < 4; i++){
@@ -61,6 +130,8 @@ int main(int argc, char **argv)
     {
         numbers[i].set_num(argv[1][i]);
     }
+
+
     
     // solve(numbers, 4);
 }
