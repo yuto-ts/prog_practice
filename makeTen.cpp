9e04493ed70590c7582348e7ad5e1ac55e9a26b9
Tsuchiya <yuto.tsuchiya@sony.com>
2019-06-14 09:23:42 +0900

    mulとdivは動作確認OK

--- makeTen.cpp
+++ makeTen.cpp
@@ -27,6 +27,7 @@ public:
     culc_fraction& sub(culc_fraction&, culc_fraction&);
     culc_fraction& mul(culc_fraction&, culc_fraction&);
     culc_fraction& div(culc_fraction&, culc_fraction&);
+    void print();
 
 private:
     int num;
@@ -58,37 +59,43 @@ culc_fraction& culc_fraction::reduce(culc_fraction& fb){
     return *this;
 }
 
-culc_fraction& culc_fraction::add(culc_fraction& fb){
-    reduce(fb);
-    num += fb.num;
+culc_fraction& culc_fraction::add(culc_fraction& fa, culc_fraction& fb){
+    fa.reduce(fb);
+    num = fa.num + fb.num;
     reduce();
+    fa.reduce();
     fb.reduce();
     return *this;
 }
 
-culc_fraction& culc_fraction::sub(culc_fraction& fb){
-    reduce(fb);
-    num -= fb.num;
+culc_fraction& culc_fraction::sub(culc_fraction& fa, culc_fraction& fb){
+    fa.reduce(fb);
+    num = fa.num - fb.num;
     reduce();
+    fa.reduce();
     fb.reduce();
     return *this;
 }
 
-culc_fraction& culc_fraction::mul(culc_fraction& fb){
-    num *= fb.num;
-    den *= fb.den;
+culc_fraction& culc_fraction::mul(culc_fraction& fa, culc_fraction& fb){
+    num = fa.num * fb.num;
+    den = fa.den * fb.den;
     reduce();
     return *this;
 }
 
-culc_fraction& culc_fraction::div(culc_fraction& fb){
-    num *= fb.den;
-    den *= fb.num;
+culc_fraction& culc_fraction::div(culc_fraction& fa, culc_fraction& fb){
+    num = fa.num * fb.den;
+    den = fa.den * fb.num;
     reduce();
     return *this;
 }
 
 
+
+void culc_fraction::print(){
+    std::cout << num << "/" << den << std::endl;
+}
 
 class culc_Ten : public culc_fraction
 {
@@ -96,7 +103,8 @@ public:
     culc_Ten& set_num(char);
     auto      set_nums(culc_Ten *numbers, char argv);
     culc_Ten& set_fraction_num(char);
-    // culc_Ten& solve();
+    // void      print();
+    // culc_Ten& culc_Ten::solve(culc_Ten& numbers, int n);
 private:
     int numb;
     std::vector<int> str{1};
@@ -121,13 +129,70 @@ culc_Ten& culc_Ten::set_fraction_num(char argv){
     return *this;
 }
 
-// culc_Ten& culc_Ten::solve(culc_Ten& num){
-    
+// culc_Ten& culc_Ten::solve(culc_Ten& numbers, int n)
+// {
+//     int i, j, k, l;
+//     struct numstr *new_numbers;
+
+//     if (n == 1) {
+//         if (fabs(numbers[0].num - 10.) < 1e-5) {
+//             printf("%s=10\n", numbers[0].str);
+//         }
+//         return;
+//     }
+
+//     new_numbers = (struct numstr *)malloc(sizeof(struct numstr) * (n-1));
+
+//     /* numbers[i]とnumbers[j]を演算 */
+//     for (i=0; i<n-1; i++) {
+//         for (j=i+1; j<n; j++) {
+//             /* 演算しないものをnew_numbersにコピー */
+//             l = 0;
+//             for (k=0; k<n; k++) {
+//                 if ((k!=i)&&(k!=j)) {
+//                     new_numbers[l++] = numbers[k];
+//                 }
+//             }
+//             /* 以下、演算結果をnew_numbers[l]に入れる */
+
+//             new_numbers[l].num = numbers[i].num + numbers[j].num;
+//             sprintf(new_numbers[l].str, "(%s+%s)", numbers[i].str, numbers[j].str);
+//             solve(new_numbers, n-1);
+
+//             new_numbers[l].num = numbers[i].num - numbers[j].num;
+//             sprintf(new_numbers[l].str, "(%s-%s)", numbers[i].str, numbers[j].str);
+//             solve(new_numbers, n-1);
+
+//             new_numbers[l].num = numbers[j].num - numbers[i].num;
+//             sprintf(new_numbers[l].str, "(%s-%s)", numbers[j].str, numbers[i].str);
+//             solve(new_numbers, n-1);
+
+//             new_numbers[l].num = numbers[i].num * numbers[j].num;
+//             sprintf(new_numbers[l].str, "(%s*%s)", numbers[i].str, numbers[j].str);
+//             solve(new_numbers, n-1);
+
+//             if (fabs(numbers[j].num) > 1e-5) {
+//                 new_numbers[l].num = numbers[i].num / numbers[j].num;
+//                 sprintf(new_numbers[l].str, "(%s/%s)", numbers[i].str, numbers[j].str);
+//                 solve(new_numbers, n-1);
+//             }
+
+//             if (fabs(numbers[i].num) > 1e-5) {
+//                 new_numbers[l].num = numbers[j].num / numbers[i].num;
+//                 sprintf(new_numbers[l].str, "(%s/%s)", numbers[j].str, numbers[i].str);
+//                 solve(new_numbers, n-1);
+//             }
+//         }
+//     }
+
+//     free(new_numbers);
 // }
+
 
 int main(int argc, char **argv)
 {
     culc_Ten numbers[4];
+    culc_Ten ans;
 
     if ((argc != 2) || (strlen(argv[1]) != 4)) {
         printf("usage: %s (4 digits number)\n", argv[0]);
@@ -139,6 +204,7 @@ int main(int argc, char **argv)
         numbers[i].set_fraction_num(argv[1][i]);
     }
 
+    ans.div(numbers[0],  numbers[1]);
     // numbers[0].solve(numbers[1]);
 
 
