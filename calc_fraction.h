6c1a7b083951abfae425e5244e7f71835636f0b2
Tsuchiya <yuto.tsuchiya@sony.com>
2019-06-14 15:53:10 +0900

    クラスをヘッダにわけた

---
+++ calc_fraction.h
@@ -0,0 +1,109 @@
+#include <iostream>
+
+int gcd(int,int);
+
+int gcd(int x, int y){
+    while(y){
+        int w = x % y;
+            x = y;
+            y = w;
+    }
+    return x;
+}
+
+class calc_fraction
+{
+public:
+    calc_fraction& set(int, int = 1);
+    calc_fraction& reduce(); // 約分
+    calc_fraction& reduce(calc_fraction&); //通分
+    calc_fraction& add(calc_fraction&, calc_fraction&);
+    calc_fraction& sub(calc_fraction&, calc_fraction&);
+    calc_fraction& mul(calc_fraction&, calc_fraction&);
+    calc_fraction& div(calc_fraction&, calc_fraction&);
+    void           print();
+    bool           lessTen(); //10比較
+    bool           zero_check();
+
+private:
+    int num;
+    int den;
+};
+
+calc_fraction& calc_fraction::set(int n, int d)
+{
+    num = n;
+    den = d;
+    reduce();
+    return *this;
+}
+
+calc_fraction& calc_fraction::reduce()
+{
+    int g = ::gcd(num, den);
+    num /= g;
+    den /= g;
+}
+
+calc_fraction& calc_fraction::reduce(calc_fraction& fb){
+    int d = den;
+    num *= fb.den;
+    den *= fb.den;
+    fb.den *= d;
+    fb.num *= d;
+    return *this;
+}
+
+calc_fraction& calc_fraction::add(calc_fraction& fa, calc_fraction& fb){
+    fa.reduce(fb);
+    num = fa.num + fb.num;
+    den = fa.den;
+    reduce();
+    fa.reduce();
+    fb.reduce();
+    return *this;
+}
+
+calc_fraction& calc_fraction::sub(calc_fraction& fa, calc_fraction& fb){
+    fa.reduce(fb);
+    num = fa.num - fb.num;
+    den = fa.den;
+    reduce();
+    fa.reduce();
+    fb.reduce();
+    return *this;
+}
+
+calc_fraction& calc_fraction::mul(calc_fraction& fa, calc_fraction& fb){
+    num = fa.num * fb.num;
+    den = fa.den * fb.den;
+    reduce();
+    return *this;
+}
+
+calc_fraction& calc_fraction::div(calc_fraction& fa, calc_fraction& fb){
+    num = fa.num * fb.den;
+    den = fa.den * fb.num;
+    reduce();
+    return *this;
+}
+
+bool calc_fraction::zero_check(){
+    if (num == 0){
+        return 0;
+    }
+    return 1;
+}
+
+void calc_fraction::print(){
+    std::cout << num << "/" << den << std::endl;
+}
+
+bool calc_fraction::lessTen(){
+    if (num == 10 && den == 1){
+        std::cout << num  << "/" << den << std::endl;
+        return 1;
+    }else{
+        return 0;
+    }
+}
---
+++ makeTen.h
@@ -0,0 +1,91 @@
+
+#include <iostream>
+#include <string>
+#include "calc_fraction.h"
+
+class calc_Ten : public calc_fraction
+{
+public:
+    calc_Ten& set_num(char);
+    auto      set_nums(calc_Ten *numbers, char argv);
+    calc_Ten& set_fraction_num(char);
+    void      solve(calc_Ten* numbers, int n);
+    void      set_str(calc_Ten&, calc_Ten&);
+
+private:
+    int numb;
+    std::string str;
+};
+
+calc_Ten& calc_Ten::set_num(char argv){
+    numb = (int)(argv  - '0');
+    str = argv;
+    return *this;
+}
+
+auto calc_Ten::set_nums(calc_Ten *numbers, char argv){
+    for (int i = 0; i < 4; i++){
+        numbers[i].set_num(argv);
+    }
+}
+
+calc_Ten& calc_Ten::set_fraction_num(char argv){
+    set_num(argv);
+    calc_fraction::set(numb);
+    return *this;
+}
+
+void calc_Ten::solve(calc_Ten* numbers, int n)
+{
+    calc_Ten* ans = new calc_Ten[10];
+
+    if (n == 1) {
+        if(numbers[0].lessTen()){
+            std::cout << numbers[0].str << " == 10" << std::endl;
+        }
+        return;
+    }
+
+    for (int i=0; i<n-1; i++) {
+        for (int j=i+1; j<n; j++) {
+            // 計算しないものをansにコピー
+            int l = 0;
+            for (int k=0; k<n; k++) {
+                if ((k!=i)&&(k!=j)) {
+                    ans[l++] = numbers[k];
+                }
+            }
+
+            // 計算結果をans[l]に入れる 
+            ans[l].add(numbers[i], numbers[j]);
+            ans[l].str = "(" + numbers[i].str + " + " + numbers[j].str + ")";
+            solve(ans, n-1);
+
+            ans[l].sub(numbers[i], numbers[j]);
+            ans[l].str = "(" + numbers[i].str + " - " + numbers[j].str + ")";
+            solve(ans, n-1);
+
+            ans[l].sub(numbers[j], numbers[i]);
+            ans[l].str = "(" + numbers[j].str + " - " + numbers[i].str + ")";
+            solve(ans, n-1);
+
+            ans[l].mul(numbers[i], numbers[j]);
+            ans[l].str = "(" + numbers[i].str + " * " + numbers[j].str + ")";
+            solve(ans, n-1);
+
+            if (numbers[j].zero_check()){
+                ans[l].div(numbers[i], numbers[j]);
+                ans[l].str = "(" + numbers[i].str + " / " + numbers[j].str + ")";
+                solve(ans, n-1);
+            }
+
+            if (numbers[i].zero_check()){
+                ans[l].div(numbers[j], numbers[i]);
+                ans[l].str = "(" + numbers[j].str + " / " + numbers[i].str + ")";
+                solve(ans, n-1);
+            }
+        }
+    }
+    delete [] ans;
+}
+
